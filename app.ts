d1fdb277712e0e6cba7379b8b17760bafb7d8257
Dobbymin <gmin990624@gmail.com>
2025-04-09 04:11:40 +0900

    fix: 테스트용 관리자 물품 목록 mock data 적용

--- app.ts
+++ app.ts
@@ -1,15 +1,23 @@
 import express, { Express, Request, Response } from 'express';
 
+import { ITEMS_MOCK } from './src/mock';
+import { specs, swaggerUi } from './swagger';
+
 const app: Express = express();
 
 const PORT: number = 8080;
 
-//Express 4.16.0버전 부터 body-parser의 일부 기능이 익스프레스에 내장 body-parser 연결
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/swagger-ui', swaggerUi.serve, swaggerUi.setup(specs));
 
 app.get('/', (_req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('반갑습니다! 해달 동아리 물품 대여 서비스 API입니다.');
+});
+
+app.get('/admin/itemList', (_req: Request, res: Response) => {
+  res.send([...ITEMS_MOCK]);
 });
 
 app.listen(PORT, () => {
