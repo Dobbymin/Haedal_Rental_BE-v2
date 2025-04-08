bdb092f66b28840aad43af69113421f84bee6d13
Dobbymin <gmin990624@gmail.com>
2025-04-08 13:36:33 +0900

    feat: typescript 설정 추가 및 alias 설정 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
-import prettierConfig from 'eslint-config-prettier';
-import prettier from 'eslint-plugin-prettier';
 import js from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import prettier from 'eslint-plugin-prettier';
+import tseslint from 'typescript-eslint';
 
-export default [
+export default tseslint.config(
   {
     ignores: [
       'dist',
@@ -14,13 +16,24 @@ export default [
       '**/*.config.cjs',
     ],
   },
-  js.configs.recommended,
-  prettierConfig,
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      prettierConfig,
+      importPlugin.flatConfigs.recommended, // import 권장 규칙
+      importPlugin.flatConfigs.typescript, // TypeScript import 권장 규칙
+    ],
+    // 적용 파일 패턴
+    files: ['**/*.ts'], // TypeScript 파일만 대상으로 설정
+  },
+
   {
     languageOptions: {
       ecmaVersion: 2020,
       sourceType: 'module',
     },
+    parser: tseslint.ESLintParser,
     plugins: {
       prettier: prettier,
     },
@@ -42,6 +55,40 @@ export default [
       'no-var': 'error',
       'prefer-const': 'error',
       'prefer-template': 'warn',
+      'import/order': [
+        'off',
+        {
+          'newlines-between': 'always', // import 사이에 한 줄 띄우기
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          groups: ['builtin', 'external', 'internal', ['parent', 'sibling'], 'object', 'type'],
+          pathGroups: [
+            {
+              pattern: 'express',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '@/**',
+              group: 'internal',
+              position: 'after',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+        },
+      ],
     },
-  },
-];
+
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.ts'],
+          moduleDirectory: ['node_modules', 'src/'],
+        },
+        typescript: {
+          alwaysTryTypes: true, // 타입스크립트 파일도 찾도록 설정
+          project: './tsconfig.json', // 프로젝트의 tsconfig.json을 사용
+        },
+      },
+    },
+  }
+);
