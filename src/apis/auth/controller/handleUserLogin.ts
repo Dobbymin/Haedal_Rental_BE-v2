17d66161f2bf1636880faf674d98018eb50324b0
Dobbymin <gmin990624@gmail.com>
2025-04-11 02:40:09 +0900

    feat(auth): 로그인 handler 구현

---
+++ src/apis/auth/controller/handleUserLogin.ts
@@ -0,0 +1,29 @@
+import { Request, RequestHandler, Response } from 'express';
+
+import { searchUser } from '../../../mock';
+import { createSession, generateAccessToken, generateRefreshToken } from '../service';
+
+export const handleUserLogin: RequestHandler = (req: Request, res: Response) => {
+  const { id, password } = req.body;
+
+  const user = searchUser(id);
+
+  if (!user || user.password !== password) {
+    // 아이디 또는 비밀번호가 DB에 없다면 404 Not Found
+    res.status(404).send('아이디 또는 비밀번호가 잘못되었습니다.');
+  }
+
+  const session = createSession(user.role);
+
+  const accessToken = generateAccessToken({
+    userId: user.userId,
+    role: user.role,
+  });
+
+  const refreshToken = generateRefreshToken();
+
+  res.setHeader('Authorization', `Bearer ${accessToken}`);
+  res.setHeader('Refresh-Token', refreshToken);
+
+  res.status(200).send(session);
+};
